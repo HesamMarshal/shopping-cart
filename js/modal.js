7c8d738a17f41d4fe2752a1210fb00d47a0bb8a6
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-20 10:59:40 +0330

    add empty modal

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+// on show modal click
+//     Modal opacity  => 1
+//     backdrop display  => block
+
+const showModalBtn = document.querySelector(".show-modal");
+const closeBtn = document.querySelector(".close-modal");
+const closeConfirmBtn = document.querySelector(".close-confirm-modal");
+
+
+const modal = document.querySelector(".modal");
+const backDrop = document.querySelector(".backdrop");
+
+showModalBtn.addEventListener('click', () => {
+    modal.style.opacity = "1";
+    modal.style.transform = "translateY(20vh)";
+    backDrop.style.display = "block";
+
+});
+
+
+function closeModal() {
+    modal.style.opacity = "0";
+    modal.style.transform = "translateY(-100vh)";
+    backDrop.style.display = "none";
+}
+closeBtn.addEventListener('click', closeModal);
+closeConfirmBtn.addEventListener('click', closeModal);
+backDrop.addEventListener('click', closeModal);
