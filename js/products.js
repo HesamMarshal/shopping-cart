984a95b99e6e075bc3ee1d142182880b5a3cd332
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-21 19:43:56 +0330

    add more products

--- js/products.js
+++ js/products.js
@@ -9,6 +9,25 @@ export const productsData = [
         id: 2,
         title: "Desk 2",
         price: 40.15,
+        imageUrl: "../images/product3.jpg",
+    },
+    {
+        id: 3,
+        title: "Desk 3",
+        price: 40.15,
+        imageUrl: "../images/product4.jpg",
+    },
+    {
+        id: 4,
+        title: "Desk 4",
+        price: 40.15,
         imageUrl: "../images/product1.jpg",
+    },
+    {
+        id: 5,
+        title: "Desk 5",
+        price: 40.15,
+        imageUrl: "../images/product3.jpg",
     }
+
 ];
