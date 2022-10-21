36e8f4261030d861573593a1b46fee2255798042
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-21 09:32:03 +0330

    add products.js file

---
+++ js/products.js
@@ -0,0 +1,14 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "Desk 1",
+        price: 30.55,
+        imageUrl: "../images/product1.jpg",
+    },
+    {
+        id: 2,
+        title: "Desk 2",
+        price: 40.15,
+        imageUrl: "../images/product1.jpg",
+    }
+];
