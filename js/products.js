85fdddb1731b12a61031707a5e53cf4c8d75c9eb
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2022-10-26 12:37:39 +0330

    add products, change product names and prices

--- js/products.js
+++ js/products.js
@@ -1,33 +1,45 @@
 export const productsData = [
     {
         id: 1,
-        title: "Desk 1",
+        title: "Gray Setup",
         price: 30.55,
         imageUrl: "../images/product1.jpg",
     },
     {
         id: 2,
-        title: "Desk 2",
-        price: 40.15,
-        imageUrl: "../images/product3.jpg",
+        title: "Mailchimp Setup",
+        price: 35.18,
+        imageUrl: "../images/product2.jpg",
     },
     {
         id: 3,
-        title: "Desk 3",
+        title: "Wooden Desk",
+        price: 28.96,
+        imageUrl: "../images/product3.jpg",
+    },
+    {
+        id: 4,
+        title: "Sepia Setup",
         price: 40.15,
         imageUrl: "../images/product4.jpg",
     },
     {
-        id: 4,
-        title: "Desk 4",
-        price: 40.15,
-        imageUrl: "../images/product1.jpg",
+        id: 5,
+        title: "Purple Setup",
+        price: 84.50,
+        imageUrl: "../images/product5.jpg",
     },
     {
-        id: 5,
-        title: "Desk 5",
-        price: 40.15,
-        imageUrl: "../images/product3.jpg",
+        id: 6,
+        title: "Casual Setup",
+        price: 39.66,
+        imageUrl: "../images/product6.jpg",
+    },
+    {
+        id: 7,
+        title: "Apple Desk",
+        price: 56.11,
+        imageUrl: "../images/product7.jpg",
     }
 
 ];
